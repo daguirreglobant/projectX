13596d7ba3490b00cafb2140b089f6b55e233c27
Daniel Aguirre Salamanca <d.aguirre@globant.com>
2022-01-28 09:05:46 -0500

    homework 4 added

---
+++ tarea-4/main.js
@@ -0,0 +1,23 @@
+import * as helpers from './helpers';
+import calculate from './calculator';
+
+let [string, stringHTML, result, resultDOM] = [
+  "",
+  "",
+  undefined,
+  document.getElementById("result"),
+];
+
+function main(text) {
+  if ((string.length == 0) & isNaN(text)) {
+    [string, stringHTML] = helpers.isContinuous(result);
+  }
+  [string, stringHTML] = helpers.addText(string, stringHTML, text);
+  if (text == "=") {
+    result = calculate(string);
+    resultDOM.innerHTML = helpers.formatNumber(result);
+    [string, stringHTML] = ["", ""];
+  } else {
+    resultDOM.innerHTML = stringHTML;
+  }
+}
